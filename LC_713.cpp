9a4c6516a4258e13f85496963001790a134f3151
ronniiitt012 <paulronit898@gmail.com>
2026-03-09 23:27:24 +0530

    Create LC_713.cpp

---
+++ LC_713.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            long product =1;
+            for(int j=i;j<n;j++){
+                product*=nums[j];
+                if(product>=k){
+                    break;
+                }
+                count++;
+            }
+        }
+        return count;
+    }                         
+};
